ea936129b417dcc96177fea1a7d0f605758a510b
Syd Sisco <hello@sydsis.co>
2021-02-17 15:46:44 -0800

    add tests for the getUserByEmail helper function

---
+++ test/helpers.test.js
@@ -0,0 +1,31 @@
+const { assert } = require('chai');
+
+const { getUserByEmail } = require('../helpers.js');
+
+const testUsers = {
+  'userRandomID': {
+    id: 'userRandomID',
+    email: 'user@example.com',
+    password: 'purple-monkey-dinosaur'
+  },
+  'user2RandomID': {
+    id: 'user2RandomID',
+    email: 'user2@example.com',
+    password: 'dishwasher-funk'
+  }
+};
+
+describe('getUserByEmail', function() {
+  it('should return a user with valid email', () => {
+    const user = getUserByEmail('user@example.com', testUsers);
+    const expectedOutput = 'userRandomID';
+    
+    assert.equal(user.id, expectedOutput);
+  });
+
+  it('should return null when the user does not exist', () => {
+    const user = getUserByEmail('doesnotexist@fake.com', testUsers);
+    
+    assert.isNull(user);
+  });
+});
